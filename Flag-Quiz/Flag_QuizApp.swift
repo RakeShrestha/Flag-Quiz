34f77a9201c32a1fe987e02dbc76f60f285133c4
RakeShrestha <orakeshstha7@gmail.com>
2024-12-23 19:40:24 +0545

    Initial Commit

---
+++ Flag-Quiz/Flag_QuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flag_QuizApp.swift
+//  Flag-Quiz
+//
+//  Created by Rakesh Shrestha on 23/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct Flag_QuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
