68eb20329b118cc89d6c7f2a88bbffe72d0ec6cf
RakeShrestha <orakeshstha7@gmail.com>
2024-12-23 20:12:07 +0545

    learning  core SwiftUI techniques

--- Flag-Quiz/ContentView.swift
+++ Flag-Quiz/ContentView.swift
@@ -9,13 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
+        ZStack(alignment: .bottomLeading) {
             Text("Hello, world!")
+            Text("This is another text!")
+            ZStack(alignment: .top) {
+                Text("Hello, world!")
+                Text("This is another text!")
+            }
         }
-        .padding()
     }
 }
 
