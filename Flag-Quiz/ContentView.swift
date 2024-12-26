eb0112c3ced941d97bd0d3e2355ed220799ce67f
RakeShrestha <orakeshstha7@gmail.com>
2024-12-26 09:58:35 +0545

    Made a fully functional quiz app that tracks score

--- Flag-Quiz/ContentView.swift
+++ Flag-Quiz/ContentView.swift
@@ -8,16 +8,111 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    @State private var score = 0
+    @State private var totalQuestions = 1
+    @State private var questionLimitExceeded = false
+    @State private var playAgainTitle = ""
+    
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"]
+    @State private var correctAnswer = Int.random(in: 0...2)
+    
     var body: some View {
-        ZStack(alignment: .bottomLeading) {
-            Text("Hello, world!")
-            Text("This is another text!")
-            ZStack(alignment: .top) {
-                Text("Hello, world!")
-                Text("This is another text!")
+        ZStack {
+            RadialGradient(stops: [
+                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3),
+            ], center: .top, startRadius: 200, endRadius: 400)
+            .ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+                Text("Guess the Flag")
+                    .font(.largeTitle.weight(.bold))
+                    .foregroundStyle(.white)
+                VStack(spacing: 15) {
+                    
+                    VStack {
+                        Text("Tap the flag of")
+                            .foregroundStyle(.secondary)
+                            .font(.subheadline.weight(.heavy))
+                        Text(countries[correctAnswer])
+                            .font(.largeTitle.weight(.semibold))
+                    }
+                    
+                    ForEach(0..<3) { number in
+                        Button {
+                            flagTapped(number)
+                        } label: {
+                            Image(countries[number])
+                                .clipShape(.capsule)
+                                .shadow(radius: 5)
+                        }
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.regularMaterial)
+                .clipShape(.rect(cornerRadius: 20))
+                
+                Spacer()
+                Spacer()
+                
+                Text("Score: \(score)")
+                    .foregroundStyle(.white)
+                    .font(.title.bold())
+                Spacer()
             }
+            .padding()
+        }
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: askQuestion)
+        } message: {
+            Text ("Your score is \(score)")
+        }
+        .alert(playAgainTitle, isPresented: $questionLimitExceeded) {
+            Button("Resart", action: reset)
+        } message: {
+            Text ("Play again?")
         }
     }
+    
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+            score += 1
+        } else {
+            scoreTitle = "Wrong! Thats the flag of \(countries[number])."
+        }
+        
+        showingScore = true
+    }
+    
+    func askQuestion() {
+        totalQuestions += 1
+        if totalQuestions == 8 {
+            playAgainTitle = "Your final score is \(score)."
+            questionLimitExceeded = true
+        }
+        else {
+            shuffle()
+        }
+    }
+    
+    func reset() {
+        score = 0
+        totalQuestions = 0
+        shuffle()
+    }
+    
+    func shuffle() {
+        
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
+    
 }
 
 #Preview {
